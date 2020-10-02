2c5b5d4c857c69cf310c57f33022069eeee9b87c
Ubuntu <ubuntu@ip-10-0-0-13.us-west-1.compute.internal>
2020-10-02 17:32:21 +0000

    Adding latest pyspark code

--- pyspark_hate_filter3.py
+++ pyspark_hate_filter3.py
@@ -5,11 +5,13 @@ import pyspark.sql.functions as psf
 from pyspark.sql.functions import col, size
 from pyspark.sql.functions import length, lower
 from pyspark.sql import functions as F
-
+from pyspark.sql.functions import udf
 from pyspark import SparkConf
 from pyspark import SparkContext
 from pyspark.sql import SQLContext
-
+from pyspark.sql.types import *
+import sys
+import re
 from dbconnector import CassandraConnector as cassandra # Loading my own cassandra connector
 
 conf = SparkConf().setMaster("local").setAppName("pyspark-hatespeech").set("spark.jars.packages", \
@@ -17,9 +19,10 @@ conf = SparkConf().setMaster("local").setAppName("pyspark-hatespeech").set("spar
 sc = SparkContext(conf=conf)
 spark=SQLContext(sc)
 
-s3_path="s3n://mahrukh-s3/tweets/tweets/hsol_modified_tweets3.tsv"
+s3_path="s3n://mahrukh-s3/tweets/tweets/*.txt"
+#s3_path="s3n://mahrukh-s3/tweets/tweets/Twitter2.tsv"
 
-lexicon_file="refined_ngram_dict3.csv"
+lexicon_file="refined_ngram_dict2.csv"
 
 def get_data(s3_path):
 	aws_profile = "myaws"
@@ -61,7 +64,25 @@ def get_lexicon(filename):
         for line in fin:
             line=line.strip()
             lst.append(line)
+    print('Length of lexicon is ', len(lst))
     return lst
+
+def build_regex(keywords):
+    res = '('
+    for key in keywords:
+        if re.match('^[a-zA-Z]+', key):
+            res += '\\b' + key + '\\b|'
+    res = res[0:len(res) - 1] + ')'
+    print(res)
+    return res
+
+
+def get_matching_string(line, regex):
+    #print('******************', line, regex[:5])
+    matches = re.findall(regex, line)
+    return matches if matches else None
+
+
 
 
 def text_lookup(df,lexicon):
@@ -73,6 +94,16 @@ def text_lookup(df,lexicon):
         psf.regexp_extract(col1, '(?=^|\s)(' + '|'.join(lexicon) + ')(?=\s|$)', 0))
     return df2
 
+def text_lookup2(df,lexicon):
+    col1='text'
+    col2='matched_text'
+    df = df.filter(df.text.isNotNull())
+    udf_func = udf(lambda line, regex: get_matching_string(line, regex),ArrayType(StringType()))
+    #df2 = df.withColumn('matched_text', udf_func(df['text'], F.lit(build_regex(lexicon)))).withColumn('count', F.size('matched_text'))
+    df2 = df.withColumn(col2, udf_func(df[col1], F.lit(build_regex(lexicon)))).withColumn('count', F.size(col2))
+
+    return df2
+
 # 1 Read data from s3
 print('Reading data from s3')
 df=get_data2(s3_path)
@@ -82,18 +113,32 @@ lexicon=get_lexicon(lexicon_file)
 
 # 3 Lookup text
 print('Applying text filters')
-df2=text_lookup(df,lexicon)
+df2=text_lookup2(df,lexicon)
 
 # 4 Filter rows where hate content was found
 print('Showing rows where text was filtered')
-result=df2.filter(length(df2.matched_text)>0)
+result=df2.filter(df2['count']>0)
 result.show()
 
 # 5 renaming columns
-result= result.select(col("id"), col("text").alias("inputtext"), col("matched_text").alias("match"))
+#id|    userid|           username|                text|            hashtags|      date|retweet_count|label|matched_text|count
+result= result.select(col("id"),col("userid"),col("username"),col("hashtags"),col("date"),col("retweet_count"),col("label"), col("text").alias("inputtext"), col("matched_text").alias("match"))
 result.show()
 
 # 5 write to cassandra
 print('Writing to cassandra')
 
-cassandra.write_df(result, table_name="textmatch", keyspace_name="hatespeech", show=True)
+cassandra.write_df(result, table_name="textmatch2", keyspace_name="hatespeech", show=True)
+
+print('Calculate daily count of hate tweets')
+count_by_dates=result.filter(result.date.isNotNull()).cube('date').count()
+count_by_dates=count_by_dates.filter(count_by_dates.date.isNotNull())
+print(count_by_dates.count())
+cassandra.write_df(count_by_dates, table_name="dailytrend", keyspace_name="hatespeech", show=True)
+
+print('Calculate daily count of overall tweets')
+overall_by_dates=df2.filter(df2.date.isNotNull()).cube('date').count()
+overall_by_dates=overall_by_dates.filter(overall_by_dates.date.isNotNull())
+print(overall_by_dates.count())
+cassandra.write_df(overall_by_dates, table_name="overalldailytrend", keyspace_name="hatespeech", show=True)
+
